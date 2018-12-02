35031b6ed2f0b0bb88ef8982aafc9072190e7a14
Akshay Sharma <akshayaxe2096@gmail.com>
2018-12-02 13:03:48 +0530

    Add multiprocessing support

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import os
 from glob import glob
-import xmlParser
+import createDocx
+import multiprocessing as mp
+import time
 
-for file in glob("PDFs/" + "*.pdf"):
-    print(file)
-    file = file.split("\\")[-1]
+def execute(file):
     dir = file[:-4]
-    print(dir)
-    try:
-        os.makedirs("XML/" + dir)
-    except:
-        pass
     os.system("pdftohtml -c -hidden -xml " + "PDFs/" + file + " " + "XML/" + dir + "/" + file + ".xml")
     path = "XML/" + dir + "/"
-    xmlParser.parseXML(path, file + ".xml")
+    createDocx.createDocx(path, file + ".xml")
+
+if __name__ == '__main__':
+    process = []
+    max_process = 4
+    PDFs = glob("PDFs/" + "*.pdf")
+    totalPDFs = len(PDFs)
+    # print(totalPDFs)
+    for i, file in enumerate(PDFs):
+        i=i+1
+        print(i, ": ", file)
+        file = file.split("\\")[-1]
+        try:
+            os.makedirs("XML/" + file[:-4])
+        except:
+            pass
+        
+        p = mp.Process(target = execute, args = (file,))
+        process.append(p)
+        if i%max_process==0:
+            for j in range(len(process)):
+                process[j].start()
+            for j in range(len(process)):
+                process[j].join()
+            process = []
+    for j in range(len(process)):
+        process[j].start()
+    for j in range(len(process)):
+        process[j].join()
+    process = []
